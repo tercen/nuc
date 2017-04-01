8e86cbed2139b7614fb66b25842a74b4b7dfff66
Faris Naji <fpnaji@gmail.com>
2017-04-01 13:15:34 +0200

    updated the paths in the scripts

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # nuc
-image microscopy of cells
+Example operators on the image microscopy of cells
+
+images are taken from the following bioconductor packages
+
+* EBimage package (2 images)
+* imageHTS package (12 wells, each with 3 images)
+
+The full dataset contained in the imageHTS package is found at the link below:
+
+http://www.ebi.ac.uk/huber-srv/cellmorph/kimorph/
+
+A subset of the data is implememted in 
+
+https://www.tercen.com/core/#w/d1ec2e9e12d64aa9017fc0e3fd3665aa
+
--- convet_image_to_tercen.R
+++ convet_image_to_tercen.R
@@ -1,21 +1,30 @@
-library(EBImage)
-library(reshape2)
+#library(EBImage)
+#library(reshape2)
 
-nuc = readImage(system.file("images", "nuclei.tif", package="EBImage"))
-display(nuc, method = "raster", all = TRUE)
-image1 <- imageData(nuc)[1:510,1:510,1]
-image2 <- imageData(nuc)[1:510,1:510,2]
-image3 <- imageData(nuc)[1:510,1:510,3]
-image4 <- imageData(nuc)[1:510,1:510,4]
-nuc1 <- melt(image1)
-nuc2 <- melt(image2)
-nuc3 <- melt(image3)
-nuc4 <- melt(image4)
+#nuc = readImage(system.file("images", "nuclei.tif", package="EBImage"))
+nuc_file      <- file.path("images", "ebi_image", "nuclei.tif")
+nuc1_file_out <- file.path("data", "ebi_image", "nuc1.tsv")
+nuc2_file_out <- file.path("data", "ebi_image", "nuc2.tsv")
+nuc3_file_out <- file.path("data", "ebi_image", "nuc3.tsv")
+nuc4_file_out <- file.path("data", "ebi_image", "nuc4.tsv")
 
-sample1 <- rep("sample1",510*510)
-sample2 <- rep("sample2",510*510)
-sample3 <- rep("sample3",510*510)
-sample4 <- rep("sample4",510*510)
+nuc = EBImage::readImage(nuc_file)
+#display(nuc, method = "raster", all = TRUE)
+
+image1 <- EBImage::imageData(nuc)[ , ,1]
+image2 <- EBImage::imageData(nuc)[ , ,2]
+image3 <- EBImage::imageData(nuc)[ , ,3]
+image4 <- EBImage::imageData(nuc)[ , ,4]
+
+nuc1 <- reshape2::melt(image1)
+nuc2 <- reshape2::melt(image2)
+nuc3 <- reshape2::melt(image3)
+nuc4 <- reshape2::melt(image4)
+
+sample1 <- rep("sample1", nrow(nuc1))
+sample2 <- rep("sample2", nrow(nuc2))
+sample3 <- rep("sample3", nrow(nuc3))
+sample4 <- rep("sample4", nrow(nuc4))
 
 nuc1 <- cbind(nuc1,sample1)
 nuc2 <- cbind(nuc2,sample2)
@@ -27,7 +36,8 @@ colnames(nuc2) <- c("x", "y", "value", "sample")
 colnames(nuc3) <- c("x", "y", "value", "sample")
 colnames(nuc4) <- c("x", "y", "value", "sample")
 
-write.table(x=nuc1, file ="nuc1.tsv", sep ="\t", col.names = TRUE, row.names = FALSE, quote = FALSE)
-write.table(x=nuc2, file ="nuc2.tsv", sep ="\t", col.names = TRUE, row.names = FALSE, quote = FALSE)
-write.table(x=nuc3, file ="nuc3.tsv", sep ="\t", col.names = TRUE, row.names = FALSE, quote = FALSE)
-write.table(x=nuc4, file ="nuc4.tsv", sep ="\t", col.names = TRUE, row.names = FALSE, quote = FALSE)
+
+write.table(x=nuc1, file = nuc1_file_out, sep ="\t", col.names = TRUE, row.names = FALSE, quote = FALSE)
+write.table(x=nuc2, file = nuc2_file_out, sep ="\t", col.names = TRUE, row.names = FALSE, quote = FALSE)
+write.table(x=nuc3, file = nuc3_file_out, sep ="\t", col.names = TRUE, row.names = FALSE, quote = FALSE)
+write.table(x=nuc4, file = nuc4_file_out, sep ="\t", col.names = TRUE, row.names = FALSE, quote = FALSE)
